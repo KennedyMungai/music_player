c9e2e9145a827b7542cbbd7157ab5439f6205d75
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-22 14:57:25 +0300

    Initial commit

---
+++ app/App.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MainAppPage = () => {
+	return (
+		<View>
+			<Text>MainAppPage</Text>
+		</View>
+	)
+}
+
+export default MainAppPage
+
+const styles = StyleSheet.create({})
