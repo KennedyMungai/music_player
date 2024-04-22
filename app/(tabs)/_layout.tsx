c5c8102dc3c751330f02046d3b319d0f87513def
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-22 16:03:29 +0300

    Made some cosmetic changes to the tavs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,23 @@
+import { colors, fontSize } from '@/constants/tokens'
 import { Tabs } from 'expo-router'
 import React from 'react'
 
 const TabsLayout = () => {
 	return (
-		<Tabs>
-			<Tabs.Screen name='favorites' />
-			<Tabs.Screen name='playlists' />
-			<Tabs.Screen name='(songs)' />
-			<Tabs.Screen name='artists' />
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: colors.primary,
+				tabBarLabelStyle: {
+					fontSize: fontSize.xs,
+					fontWeight: '500'
+				},
+				headerShown: false
+			}}
+		>
+			<Tabs.Screen name='favorites' options={{ title: 'Favorites' }} />
+			<Tabs.Screen name='playlists' options={{ title: 'Playlists' }} />
+			<Tabs.Screen name='(songs)' options={{ title: 'Songs' }} />
+			<Tabs.Screen name='artists' options={{ title: 'Artists' }} />
 		</Tabs>
 	)
 }
