d19c2d974e18ab62b835b37c7b8e57c12437297e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-23 09:50:13 +0300

    Added the track data to the component

--- components/track-list-item.tsx
+++ components/track-list-item.tsx
@@ -1,9 +1,39 @@
+import { unknownTrackImageUri } from '@/constants/images'
 import React from 'react'
+import { StyleSheet, TouchableHighlight, View } from 'react-native'
+import FastImage from 'react-native-fast-image'
 
-type Props = {}
+type Props = {
+	track: ITrack
+}
 
-const TrackListItem = (props: Props) => {
-	return <div>TrackListItem</div>
+const TrackListItem = ({ track }: Props) => {
+	const isActiveTrack = false
+
+	return (
+		<TouchableHighlight>
+			<View>
+				<FastImage
+					source={{
+						uri: track.artwork ?? unknownTrackImageUri,
+						priority: FastImage.priority.normal
+					}}
+					style={[
+						styles.trackArtworkImage,
+						{ opacity: isActiveTrack ? 0.6 : 1 }
+					]}
+				/>
+			</View>
+		</TouchableHighlight>
+	)
 }
 
 export default TrackListItem
+
+const styles = StyleSheet.create({
+	trackArtworkImage: {
+		borderRadius: 8,
+		width: 50,
+		height: 50
+	}
+})
