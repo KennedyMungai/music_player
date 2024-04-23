cdc753f9312daecea17a03e024731859f1c3e60b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-23 10:00:58 +0300

    Rendered out the track list

--- components/track-list.tsx
+++ components/track-list.tsx
@@ -1,8 +1,14 @@
 import { FlatList } from 'react-native'
 import data from '../assets/data/library.json'
+import TrackListItem from './track-list-item'
 
 const TrackList = () => {
-	return <FlatList data={data as ITrack[]} renderItem={() => null} />
+	return (
+		<FlatList
+			data={data as ITrack[]}
+			renderItem={({ item: track }) => <TrackListItem track={track} />}
+		/>
+	)
 }
 
 export default TrackList
