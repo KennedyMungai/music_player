e3fe4926c4396c86f66c465eee42a45eab758e08
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-23 10:54:57 +0300

    Added the import for the utilStyles stylkes

--- components/track-list.tsx
+++ components/track-list.tsx
@@ -1,8 +1,19 @@
-import { FlatList, FlatListProps } from 'react-native'
+import { FlatList, FlatListProps, View } from 'react-native'
 import data from '../assets/data/library.json'
 import TrackListItem from './track-list-item'
+import { utilStyles } from '@/styles'
 
 export type TracksListProps = Partial<FlatListProps<unknown>>
+
+const ItemDivider = () => (
+	<View
+		style={{
+			...utilStyles.itemSeparator,
+			marginVertical: 9,
+			marginLeft: 16
+		}}
+	/>
+)
 
 const TrackList = ({ ...flatlistProps }: TracksListProps) => {
 	return (
