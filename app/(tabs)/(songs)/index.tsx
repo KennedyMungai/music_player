26759af706cc2c6df16de5b75507344dcff987b3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-23 11:21:56 +0300

    Using the searchbar for somethings

--- app/(tabs)/(songs)/index.tsx
+++ app/(tabs)/(songs)/index.tsx
@@ -1,10 +1,17 @@
 import TrackList from '@/components/track-list'
 import { screenPadding } from '@/constants/tokens'
+import useNavigationSearch from '@/hooks/useNavigationSearch'
 import { defaultStyles } from '@/styles'
 import React from 'react'
 import { ScrollView, View } from 'react-native'
 
 const SongsPage = () => {
+	const search = useNavigationSearch({
+		searchBarOptions: {
+			placeholder: 'Find in songs'
+		}
+	})
+
 	return (
 		<View
 			style={[
