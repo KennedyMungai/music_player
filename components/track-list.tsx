d78bed4b329b08f5be3966c4ceeab4698f32b7d4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-23 09:44:32 +0300

    Defined the type for the track list

--- components/track-list.tsx
+++ components/track-list.tsx
@@ -2,7 +2,7 @@ import { FlatList } from 'react-native'
 import data from '../assets/data/library.json'
 
 const TrackList = () => {
-	return <FlatList data={data} renderItem={() => null} />
+	return <FlatList data={data as ITrack[]} renderItem={() => null} />
 }
 
 export default TrackList
