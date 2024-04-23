76328cada8a4659dc4d9137d77860ba913c85db2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-23 10:55:20 +0300

    Reorganized the imports for the component

--- components/track-list.tsx
+++ components/track-list.tsx
@@ -1,7 +1,7 @@
+import { utilStyles } from '@/styles'
 import { FlatList, FlatListProps, View } from 'react-native'
 import data from '../assets/data/library.json'
 import TrackListItem from './track-list-item'
-import { utilStyles } from '@/styles'
 
 export type TracksListProps = Partial<FlatListProps<unknown>>
 
