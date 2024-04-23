e8fa18674bc0c7008fb6c5cc3d35740eba972bb6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-23 09:03:34 +0300

    Spread put the StackScreenWithSearchBar options

--- app/(tabs)/(songs)/_layout.tsx
+++ app/(tabs)/(songs)/_layout.tsx
@@ -1,11 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
 import { Stack } from 'expo-router'
+import React from 'react'
+import { StackSCreenWithSearchBar } from '../../../constants/layout'
 
 const SongsScreenLayout = () => {
 	return (
 		<Stack>
-			<Stack.Screen name='index' options={{ headerTitle: 'Songs' }} />
+			<Stack.Screen
+				name='index'
+				options={{ headerTitle: 'Songs', ...StackSCreenWithSearchBar }}
+			/>
 		</Stack>
 	)
 }
