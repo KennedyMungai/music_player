941eac8d8a186c5b17d2abc28bf95abaa94952f6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-23 09:22:07 +0300

    Added a flatlist fot the component

--- components/track-list.tsx
+++ components/track-list.tsx
@@ -1,7 +1,9 @@
+import { FlatList } from 'react-native'
+
 type Props = {}
 
 const TrackList = (props: Props) => {
-	return <div>TrackList</div>
+	return <FlatList data={[]} renderItem={() => null} />
 }
 
 export default TrackList
